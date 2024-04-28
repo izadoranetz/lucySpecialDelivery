aef75710847118b702b4a8a4f296c2f54bed0c40
izadoranetz <izadora.ns@gmail.com>
2024-04-27 21:04:06 -0300

    fix git ignore e comentário no menu

--- menu.py
+++ menu.py
@@ -28,9 +28,11 @@ def opcoes_menu():
         elif opcao == 3:
             empresas_cadastradas = gerenciar_empresas.gerenciar_empresas()
         elif opcao == 4:
+            # vai receber empresas_cadastradas e drones_cadastrados
             print('Gerenciar remessas')
         elif opcao == 5:
             print('Finalizando programa. Tchau!')
+            # para consulta das empresas cadastradas no outro módulo
             # for i, empresa in enumerate(empresas_cadastradas, 1):
             #     print(f'Empresa {i}:')
             #     print(f'Nome: {empresa.nome}\nCNPJ: {empresa.cnpj}\nEndereço: {empresa.endereco}\n')
